2a9f5f2e3e07a7957f318aae764f691e84c98f0f
Carol Hsu <sheseee@gmail.com>
2013-04-12 11:04:28 +0800

    building the connection with mongodb -- notice settings ORDER @ app.js

--- app.js
+++ app.js
@@ -4,24 +4,24 @@
  */
 
 var express = require('express')
-  , routes = require('./routes')
   , user = require('./routes/user')
   , http = require('http')
-  , path = require('path')
-  , db = require('./db');
+  , path = require('path');
 
-var app = express();
+var app = module.exports = express();
+
+require('./db');
 
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(express.favicon());
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
-app.use(express.methodOverride());
+// app.use(express.methodOverride());
 app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
 
@@ -29,10 +29,12 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
+var routes = require('./routes')
 
 app.get('/', routes.index);
 app.get('/users', user.list);
 app.get('/users/:number', user.one);
+app.post('/create', routes.create);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -9,3 +9,12 @@ var Todo = mongoose.model('Todo');
 exports.index = function(req, res){
   res.render('index', { title: 'Express todo list example' });
 };
+
+exports.create = function(req, res){
+	new Todo({
+		content : req.body.content,
+		updated_at : Date.new()
+	}).save(function(err, todo, count){
+		res.redirect('/');
+	});
+}
